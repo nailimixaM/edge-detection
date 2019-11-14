06553d11c5ee2fec91b47f228a1a93c4b0f8a8da
M.L. Croci <mlc70@eng.cam.ac.uk>
2019-11-14 18:15:04 +0000

    Clean up threshold

--- threshold.py
+++ threshold.py
@@ -10,65 +10,10 @@
 
 import os
 import sys
-import cv2
 import scipy
 from scipy import ndimage
 import matplotlib.pyplot as plt
 import numpy as np
-
-def neighbours(X, node): #Returns list of coords of neighbours
-    n_rows = len(X)
-    n_cols = len(X[0])
-    row = node[0]
-    col = node[1]
-
-    neigh_nodes = [(i,j) for i in range(row-1,row+2,1) for j in range(col-1,col+2,1) if i < n_rows and j < n_cols and i >= 0 and j >= 0 and (i,j) != node]
-    return neigh_nodes
-
-def update(X, node, node_visit_status, tent_dist):
-    node_neighbours = neighbours(X, node)
-    for n in node_neighbours:
-        if node_visit_status[n] == False:
-            dist = tent_dist[node] + X[n]
-            if dist < tent_dist[n]:
-                tent_dist[n] = dist
-    
-    node_visit_status[node] = True
-
-
-def algo(X, initial, target, node_visit_status, tent_dist):
-    print('Starting algorithm' + '#'*20)
-    next_node = initial 
-    while next_node != target:
-        update(X, next_node, node_visit_status, tent_dist)
-
-        next_node = ()
-        min_dist = 2000
-        for node, status in node_visit_status.items():
-            if status == False:
-                if tent_dist[node] < min_dist:
-                    next_node = node
-                    min_dist = tent_dist[node]
-        
-        if not next_node:
-            return
-
-
-def path(tent_dist, initial, target):
-    path_nodes = [target]
-    next_node = target
-    while next_node != initial:
-        min_val = 1000
-        next_neigh = neighbours(tent_dist, next_node)
-        for neigh in next_neigh:
-            if tent_dist[neigh] < min_val:
-                next_node = neigh
-                min_val = tent_dist[neigh]
-        
-        path_nodes.append(next_node)
-
-    return path_nodes
-
 
 def parse_args():
     flags = {
@@ -129,7 +74,9 @@ def main():
         picnames = ['pictures/OH0000' + str(i+1) for i in range(9)]
         picnames = picnames + ['pictures/OH000' + str(i+10) for i in range(90)]
         picnames = picnames + ['pictures/OH00' + str(i+100) for i in range(900)]
+    
 
+    #Thresholding algorithm
     for index in range(1,1000):
         fdata = data_dir + 'OH' + str(index) + '.dat'
         if not os.path.exists(fdata):
@@ -147,11 +94,12 @@ def main():
         plt.clf()
 
         #Threshold
+        thresh = 1
         edge_i = []
         edge_j = []
         for i in range(len(mag)):
             for j in range(len(mag[0])):
-                if mag[i][j] >= 1:
+                if mag[i][j] >= thresh:
                     edge_i.append(i)
                     edge_j.append(j)
         
